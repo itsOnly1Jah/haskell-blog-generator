d6281ce608b867e4b88aaa97efe243c16b8b2450
itsOnly1Jah <kingjah23@gmail.com>
2024-01-14 17:14:20 -0500

    feat: create new functions for head and title tags (3.1)

--- hello.hs
+++ hello.hs
@@ -2,3 +2,5 @@ main = putStrLn myhtml
 myhtml = html_ (body_ "Hello, World!")
 html_ content = "<html>" <> content <> "</html>"
 body_ content = "<body>" <> content <> "</body>"
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
