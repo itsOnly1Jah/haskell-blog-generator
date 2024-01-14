6d068737b2bbd1b3ffd929892a74ba37117b55c0
itsOnly1Jah <kingjah23@gmail.com>
2024-01-14 18:28:11 -0500

    feat: add type definition for 'title_' and 'makehtml' functions (3.2)

--- hello.hs
+++ hello.hs
@@ -2,8 +2,12 @@ main = putStrLn myhtml
 
 myhtml = makehtml "My page title" "My page content"
  
+makehtml :: String -> String -> String
 makehtml title content = html_ (head_ (title_ title) <> body_ content)
+
 html_ content = "<html>" <> content <> "</html>"
 body_ content = "<body>" <> content <> "</body>"
 head_ content = "<head>" <> content <> "</head>"
+
+title_ :: String -> String
 title_ content = "<title>" <> content <> "</title>"
