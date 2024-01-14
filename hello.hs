ab0e3546bd7a5f872f7c464b61b7b123b4edc460
itsOnly1Jah <kingjah23@gmail.com>
2024-01-14 18:15:28 -0500

    feat: create new makehtml function (3.1)

--- hello.hs
+++ hello.hs
@@ -1,5 +1,8 @@
-main = putStrLn myhtml 
-myhtml = html_ (body_ "Hello, World!")
+main = putStrLn myhtml
+
+myhtml = makehtml "My page title" "My page content"
+ 
+makehtml title content = html_ (head_ (title_ title) <> body_ content)
 html_ content = "<html>" <> content <> "</html>"
 body_ content = "<body>" <> content <> "</body>"
 head_ content = "<head>" <> content <> "</head>"
