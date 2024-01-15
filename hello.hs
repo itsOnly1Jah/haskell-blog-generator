c131fb67f1a44307b17e62af4514481dc522d21d
itsOnly1Jah <kingjah23@gmail.com>
2024-01-15 06:28:55 -0500

    feat: create additional functions for heading and paragraph tags (3.2)

--- hello.hs
+++ hello.hs
@@ -17,6 +17,12 @@ head_ = el "head"
 title_ :: String -> String
 title_ = el "title"
 
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
