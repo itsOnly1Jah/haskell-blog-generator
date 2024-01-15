536151cf58cf81d68d93838fd271e20a7c02e80d
itsOnly1Jah <kingjah23@gmail.com>
2024-01-15 06:01:20 -0500

    feat: partially apply el function (3.2)

--- hello.hs
+++ hello.hs
@@ -5,9 +5,17 @@ myhtml = makehtml "My page title" "My page content"
 makehtml :: String -> String -> String
 makehtml title content = html_ (head_ (title_ title) <> body_ content)
 
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
 head_ content = "<head>" <> content <> "</head>"
 
 title_ :: String -> String
 title_ content = "<title>" <> content <> "</title>"
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
